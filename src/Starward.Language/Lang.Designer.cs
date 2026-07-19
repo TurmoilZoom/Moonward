88a230ad9da78df96207c00f67c544d2e51278d9
Eric-Joker <60310368+Eric-Joker@users.noreply.github.com>
2026-07-19 19:31:09 +0800

    update starrail daily note UI (#1882)

--- src/Starward.Language/Lang.Designer.cs
+++ src/Starward.Language/Lang.Designer.cs
@@ -1169,6 +1169,15 @@ namespace Starward.Language {
         }
         
         /// <summary>
+        ///   查找类似 Accumulated Points 的本地化字符串。
+        /// </summary>
+        public static string DailyNoteButton_StarRail_AccumulatedPoints {
+            get {
+                return ResourceManager.GetString("DailyNoteButton_StarRail_AccumulatedPoints", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   查找类似 All Finished 的本地化字符串。
         /// </summary>
         public static string DailyNoteButton_StarRail_AllFinished {
@@ -1205,7 +1214,7 @@ namespace Starward.Language {
         }
         
         /// <summary>
-        ///   查找类似 Echo of War 的本地化字符串。
+        ///   查找类似 Echo of War Remaining 的本地化字符串。
         /// </summary>
         public static string DailyNoteButton_StarRail_EchoOfWar {
             get {
